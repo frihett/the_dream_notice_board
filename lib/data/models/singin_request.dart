20648ac15def577ac0fc556dfce2245543e8f9d1
sseung <sseutta@naver.com>
2025-05-14 12:26:57 +0900

    feat : 로그인 요청 모델 추가

---
+++ lib/data/models/singin_request.dart
@@ -0,0 +1,11 @@
+class SignInRequest {
+  final String username;
+  final String password;
+
+  SignInRequest({required this.username, required this.password});
+
+  Map<String, dynamic> toJson() => {
+    'username': username,
+    'password': password,
+  };
+}
